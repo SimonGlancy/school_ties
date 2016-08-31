fefff78c5b41df7189efed79275d51c9887e8e5d
Simon T Glancy <si.glancy@gmail.com>
2016-09-01 00:51:37 +0100

    schools tested all passing

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,26 @@
+class SchoolsController < ApplicationController
+  def index
+    @schools = School.all
+  end
+
+  def new
+    @school = School.new
+  end
+
+  def create
+    School.create(school_params)
+    redirect_to "/schools"
+  end
+
+  def show
+    @school = School.find(params[:id])
+  end
+
+  private
+
+  def school_params
+    params.require(:school).permit(:name)
+  end
+
+
+end
--- spec/feature/schools_feature_spec.rb
+++ spec/feature/schools_feature_spec.rb
@@ -6,7 +6,44 @@ feature 'schools' do
     scenario 'should display a prompt to add a school' do
       visit '/schools'
       expect(page).to have_content 'No schools yet'
-      expect(page).to have_link 'Add a school'
+      expect(page).to have_link 'Add a School'
+    end
+  end
+
+  context 'school can be created' do
+    scenario 'should show all schools' do
+      visit '/schools'
+      expect(page).to have_content 'No schools yet'
+      expect(page).to have_link 'Add a School'
+      click_link 'Add a School'
+      fill_in 'Name', with: 'London Grammar'
+      click_button 'Create School'
+      expect(page).to have_content 'London Grammar'
+      expect(current_path).to eq '/schools'
+    end
+  end
+
+  context 'viewing schools' do
+
+    let!(:ldng){ School.create(name:'London Grammar') }
+
+    scenario 'lets a user view a school with no attendees message' do
+     visit '/schools'
+     click_link 'London Grammar'
+     expect(page).to have_content 'London Grammar'
+     expect(page).to have_content 'No Attendees yet'
+     expect(current_path).to eq "/schools/#{ldng.id}"
+    end
+
+    scenario 'lets a user view a school with attendees' do
+     simon = Member.create(name:"Simon", email: "simon@email.com")
+     simon.attendances.create(school: ldng)
+     visit '/schools'
+     click_link 'London Grammar'
+     expect(page).to have_content 'London Grammar'
+     expect(page).to have_content simon.name
+     expect(page).to have_content simon.email
+     expect(current_path).to eq "/schools/#{ldng.id}"
     end
   end
 end
